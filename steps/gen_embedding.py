2817808db05eca8ff159e410dc35359205635496
Xinyue-Lu <77259528+Xinyue-Lu@users.noreply.github.com>
2023-04-22 17:52:53 -0400

    feature extraction and model training

---
+++ steps/gen_embedding.py
@@ -0,0 +1,78 @@
+import numpy as np
+import pandas as pd
+import argparse
+from pathlib import Path
+import os
+from sklearn.decomposition import PCA
+
+# Get chromosome length
+def GetChrLength(chrom_size, chrom):
+    tab = pd.read_table(chrom_size, header=None, sep='\t')
+    tab.columns = ['chr', 'size']
+    
+    return np.max(tab['size'][tab['chr'] == chrom])
+    
+# Read file
+def ReadBedFile(file, chr_len):
+    unit_info = np.zeros(chr_len)
+    with open(file, 'r') as f:
+        count = 0
+        for l in f:
+            if count == 0:
+                count += 1
+                continue
+            line = l.strip('\n').split('\t')
+            unit_info[int(line[1]):int(line[2])+1] += 1
+            
+            count += 1
+    return unit_info
+
+def binning_chr(unit_info, res):
+	binned_unit_info = np.zeros(int(np.ceil(len(unit_info) / res)))
+
+	for i in range(len(binned_unit_info)):
+		binned_unit_info[i] = np.sum(unit_info[i * res:min((i + 1) * res, len(unit_info))])
+
+	return binned_unit_info
+
+def dim_reduction(X, k):
+	pca = PCA(n_components=k)
+	X_pca = pca.fit_transform(X)
+	print("Explained variance ratio:", pca.explained_variance_ratio_)
+
+	return X_pca
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--data_dir', type=Path)
+	parser.add_argument('--chr_size_dir', type=Path)
+	parser.add_argument('--res', type=int)
+	parser.add_argument('--nsamples', type=int)
+
+	args = parser.parse_args()
+	data_dir = args.data_dir
+	chr_size_dir = args.chr_size_dir
+	res = args.res
+	nsamples = args.nsamples
+	
+	embed_chr = []
+	chrom_list = ["chr%d"%(i+1) for i in range(22)]
+	print(chrom_list)
+
+	for chrom in chrom_list:
+		embed = []
+		chr_len = GetChrLength(chr_size_dir / "hg38.chrom.sizes.txt", chrom)
+		read_dir = data_dir / chrom
+		print(chrom, chr_len)
+		for bedfile in sorted(os.listdir(read_dir)):
+			print("Find bedfile:", bedfile)
+			unit_info = ReadBedFile(read_dir / bedfile, chr_len)
+			binned_unit_info = binning_chr(unit_info, res)
+			embed.append(binned_unit_info)
+		embed_concat = np.concatenate(embed, axis=0).reshape((nsamples, len(binned_unit_info)))
+		embed_pca = dim_reduction(embed_concat, 5)
+		embed_chr.append(embed_pca)
+
+	chr_features = np.concatenate(embed_chr, axis=1)
+	np.save("chr_features.npy", chr_features)
+
---
+++ steps/predict.py
@@ -0,0 +1,54 @@
+import numpy as np
+import pandas as pd
+
+from sklearn.model_selection import KFold
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+import argparse
+
+def ReadFeatureAndLabel(feature_file, label_file):
+	feature = np.load(feature_file)
+	label = pd.read_csv(label_file, index_col=0, header=0)
+	label = label['label']
+
+	return feature, label     
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--feature_file', type=str)
+	parser.add_argument('--label_file', type=str)
+	parser.add_argument('--model_name', type=str)
+	parser.add_argument('--nsplit', type=int)
+
+	args = parser.parse_args()
+	feature_file = args.feature_file
+	label_file = args.label_file
+	model_name = args.model_name
+	nsplit = args.nsplit
+
+	kf = KFold(n_splits=5)
+	if model_name == 'lr':
+		model = LogisticRegression()
+	if model_name == 'svm':
+		model = SVC()
+	if model_name == 'rf':
+		model = RandomForestClassifier()
+	
+	# Load feature
+	X, y = ReadFeatureAndLabel(feature_file, label_file)
+	
+	accuracies = []
+	for train_index, test_index in kf.split(X):
+		X_train, X_test = X[train_index], X[test_index]
+		y_train, y_test = y[train_index], y[test_index]
+		model.fit(X_train, y_train)
+		y_pred = model.predict(X_test)
+		accuracy = accuracy_score(y_test, y_pred)
+		accuracies.append(accuracy)
+	
+	print(f"{nsplit} fold cross validation")
+	print(f"Accuracy: {sum(accuracies) / len(accuracies)}")
+
